1adf9f4548f3e9b0067aed34607bec2f62f34454
mrange <marten_range@hotmail.com>
2015-11-01 20:18:01 +0100

    Use fsi.exe instead

--- site/webserver.fsx
+++ site/webserver.fsx
@@ -2,22 +2,28 @@
 // Start up Suave.io
 // --------------------------------------------------------------------------------------
 
-#r "../packages/FAKE/tools/FakeLib.dll"
 #r "../packages/Suave/lib/net40/Suave.dll"
 #r "../packages/M3.MiniJson/lib/net40-client/MiniJson.dll"
+#r "FSharp.Compiler.Interactive.Settings"
 
 #load "WebPartT.fs"
 #load "MyService.fs"
 
-open Fake
+open Microsoft.FSharp.Compiler.Interactive
+
 open Suave
 open Suave.Http.Successful
 open Suave.Web
 open Suave.Types
 open System.Net
 
+let port =
+  let args = Settings.fsi.CommandLineArgs
+  if args.Length < 2 then uint16 8083
+  else
+    args.[1] |> Sockets.Port.Parse
+
 let serverConfig =
-    let port = getBuildParamOrDefault "port" "8083" |> Sockets.Port.Parse
     { defaultConfig with bindings = [ HttpBinding.mk HTTP IPAddress.Loopback port ] }
 
 startWebServer serverConfig MyService.App
